a9a5e0b93948563f18e7365b1a77bb3d48b860ad
Dafferianto Trinugroho <daft@equinor.com>
2020-03-31 08:48:56 +0200

    Patch kured with webhook URL if active cluster

---
+++ patch-kured-if-active-cluster.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# USAGE
+#
+# ./patch-kured-if-active-cluster.sh "$radixOperatorPath" "$kuredPath" "$radixPatch"
+
+#######################################################################################
+### CONFIG & VALIDATE
+### 
+
+radixOperatorPath="$1"
+kuredPath="$2"
+radixPatch="$3"
+
+# Validate input
+# if [ ! -f "$kuredPath" ]; then
+#    echo "[$(basename -- $0)] cannot find kuredPath: $kuredPath" >&2
+#    exit 1
+# fi
+if [ -z "$radixPatch" ]; then
+   echo "[$(basename -- $0)] is missing input: \"patch\"" >&2
+   exit 1
+fi
+
+
+#######################################################################################
+### MAIN
+### 
+
+# Read and store contents of cluster config as yaml
+##kubectl get cm "radix-platform-config" -o jsonpath='{.data.platform}' > tmp-cluster-config.yaml
+# Transform cluster-config yaml to shell environment variables script
+##awk '{sub(/: /,"=")}1' tmp-cluster-config.yaml > tmp-cluster-config.env && chmod +x tmp-cluster-config.env
+
+# Make the variables available for the script
+##source ./tmp-cluster-config.env
+
+# Are we in the active cluster? Use grep (if available) or similar tool to very brutish do a string search
+result="$(grep "$clusterName" "$radixOperatorPath")"
+
+if [[ -z "$result" ]]; then
+   # Found nothing, do nothing
+   echo "$clusterName is not the active cluster. Not adding Slack Webhook URL."
+   :
+else
+   # Flux is running in the active cluster
+   # Add slackUrl as a kured patch in "$radixOperatorPath/radix-patch.yaml"
+   cat "$kuredPath" >> "$radixPatch"
+   echo "      slack-hook-url: https://something.somewhere" >> "$radixPatch"
+fi
