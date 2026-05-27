8029d9395fcf367dacc11c36b282553a5799981e
Svein-Petter Johnsen <83902071+sveinpj@users.noreply.github.com>
2026-05-27 10:33:44 +0200

    Refactor check-version workflow and update PR creation script to include zone-specific details

--- .github/workflows/scripts/create_pr.sh
+++ .github/workflows/scripts/create_pr.sh
@@ -9,25 +9,41 @@ MAX_RETRIES=3
 function create-pr() {
     retry_nr=$1
     sleep_before_retry=$(($retry_nr * 2))
+    date_stamp="$(date +%Y-%m-%d)"
     if [[ -z "${PR_BRANCH}" ]]; then
         PR_BRANCH="flux-image-updates"
     fi
 
     if [[ -z "${PR_NAME}" ]]; then
-        PR_NAME="Automatic Pull Request"
+        if [[ -n "${ZONE}" ]]; then
+            PR_NAME="Automatic Pull Request - ${ZONE} - ${date_stamp}"
+        else
+            PR_NAME="Automatic Pull Request"
+        fi
+    fi
+
+    if [[ -z "${PR_BODY}" ]]; then
+        if [[ -n "${ZONE}" ]]; then
+            PR_BODY=$'**Automatic Pull Request**\n\nZone: '${ZONE}$'\nLast updated: '${date_stamp}
+        else
+            PR_BODY=$'**Automatic Pull Request**\n\nLast updated: '${date_stamp}
+        fi
     fi
 
     if [[ $(git fetch origin && git branch --remotes) == *"origin/${PR_BRANCH}"* ]]; then
         git switch "${PR_BRANCH}"
         git pull
         PR_STATE=$(gh pr view ${PR_BRANCH} --json state --jq '.state')
+        if [[ "${PR_STATE}" == "OPEN" ]]; then
+            gh pr edit ${PR_BRANCH} --title "${PR_NAME}" --body "${PR_BODY}" >/dev/null
+        fi
     else
         PR_STATE="NONEXISTENT"
     fi
 
     if [[ "${PR_STATE}" != "OPEN" ]]; then
         echo "Create PR"
-        PR_URL=$(gh pr create --title "${PR_NAME}" --base master --body "**Automatic Pull Request**")
+        PR_URL=$(gh pr create --title "${PR_NAME}" --base master --body "${PR_BODY}")
         if [[ ${PR_URL} ]]; then
             curl --request POST \
                 --header 'Content-type: application/json' \
